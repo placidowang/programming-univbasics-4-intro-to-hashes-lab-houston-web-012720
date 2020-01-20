aa2551c655b6349f60b341d9254bbc2dd6ebec3b
Placido Wang <placidow@gmail.com>
2020-01-20 18:52:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,9 +33,9 @@ end
 def update_counting_hash(hash, key)
   if hash[key]
     hash[key] += 1
-    return hash[key]
+    puts hash[key]
   else
     hash[key] = 1
-    return hash[key]
+    puts hash[key]
   end
 end
